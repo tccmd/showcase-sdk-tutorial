67855694b0b9c0a8d2f706ff0b5737c79029b164
tccmd <tccmd52@gmail.com>
2023-06-30 17:09:15 +0900

    0630 5:08 링크공유, 전체화면

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
 const showcase = document.getElementById('showcase') as HTMLIFrameElement;
-const key = 'YOUR_KEY_HERE';
+const key = 'rc4xi6txw1h0igsqa62stbtdc';
 
 // declare this file is a module
-export {};
+export { };
 
 // augment window with the MP_SDK property
 declare global {
@@ -11,16 +11,27 @@ declare global {
   }
 }
 
-showcase.addEventListener('load', async function() {
+showcase.addEventListener('load', async function () {
   let sdk;
   try {
     sdk = await showcase.contentWindow.MP_SDK.connect(showcase, key, '3.6');
   }
-  catch(e) {
+  catch (e) {
     console.error(e);
     return;
   }
 
   console.log('%c  Hello Bundle SDK! ', 'background: #333333; color: #00dd00');
   console.log(sdk);
+  // ###################################################################
+  // link
+  await sdk.Link.setShareLinkPolicy(sdk.Link.CreationPolicy.WINDOW);
+  const link = await sdk.Link.createLink();
+  console.log(link); // should log a link to your page that embeds Showcase
+  console.log("링크 바꾸기 성공");
+  await sdk.Asset.registerTexture('customTextureId0', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSdBfj0GdWn5WFn8bT8p5SVugeJOwAbBJfZPKBc9Kc&s');
+  await sdk.Pointer.editTexture('customTextureId0');
+  console.log("포인터 바꾸기 성공");
+  // ###################################################################
+
 });
